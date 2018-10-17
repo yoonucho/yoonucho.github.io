712808ab334df4d62b314d6895df810fcfe847c3
yoonucho <yoonu78@naver.com>
2018-10-17 16:29:03 +0900

    gnb 버튼 아이콘 변경 스크립트 추가밑 닫기

--- portfolio/js/common.js
+++ portfolio/js/common.js
@@ -1,19 +1,20 @@
 $(document).ready(function () {
-	menuOpen();
+	menuOpen();	
 })
 
 // 헤더 모바일  gnb메뉴 클릭이벤트
 function menuOpen() {
 	$(".gnb_btn").on("click", function () {
 
+		$("i", this).toggleClass("fa-times fa-bars ");
 		if ($(".header.mobile").hasClass("active")) {
 			$(".header.mobile").removeClass("active");
 			
 		}
 		else {
+			// alert("버튼온")
 			$(".header.mobile").addClass("active");
-			alert("버튼온")
 		}
-
 	});
 }
+
